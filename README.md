1b3de1e37b7b2a6f5b41e066fbf3fb12365b4c12
davi33-emanoel <emanoeldavi348@gmail.com>
2023-02-21 18:12:23 -0300

    re-upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# Monitor climático
+
+Neste projeto foi construído um monitor climático,
+utilizando JavaScript/HTML e CSS. Para seu funcionamento
+utilizou-se 3 API'S sendo a primeira Weather API 
+https://openweathermap.org/api, (necessário fazer cadastro
+no plano Free para conseguir sua Key). A segunda é a Country Flag https://countryflagsapi.com
+utilizada para mostrar as bandeiras de cada país,conforme a cidade na barra de busca
+e, por por último, a Unsplash https://source.unsplash.com que faz a buscas
+das imagens que aparecem no background de acordo com a cidade pesquisada.
+
+Para utilizar o projeto clone-o no seu pc e realize o cadastro na API Weather para obter sua KEY.
+## Demo
+Por motivos de segurança o projeto não funciona no modo pages, então segue o video de funcionamento:
+
+https://user-images.githubusercontent.com/79283588/190292325-285d65b7-7f6c-48c6-ad41-c09ddc922ae5.mp4
+
+## Instalação
+
+
+
+
+
+Rode o projeto pelo método de clone disponível neste respositório.
+
+```bash
+  git clone https://github.com/davi33-emanoel/monitor-de-clima.git
+```
+Depois de clonado na pasta desejada, abra o vscode e 
+na pasta js crie um arquivo :
+```bash
+    config.js
+```
+Coloque sua key conforme demonstrado no arquivo:
+ ```bash 
+  exemploConfig.js
+``` 
+Em seu vscode, abra o arquivo index.html para visualizar o projeto em seu navegador.
+## Uso
+
+Digite no campo a cidade para a qual deseja visualizar o clima, conforme demonstrado abaixo.
+
+<p textAlign="center">
+<img width="800" src="./assets/CWB.png">
+</p>
---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+//Variáveis e seleção de elementos//
+const apikey = config.KEY;
+const apitCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempELement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const humidityElement = document.querySelector("#humidity span");
+const countryElement = document.querySelector("#country");
+const windELement = document.querySelector("#wind span");
+const weatherContainer = document.querySelector("#weather-data");
+
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
+
+//Funções
+
+// Loader
+const toggleLoader = () => {
+    loader.classList.toggle("hide");
+  };
+
+const getWeatherData = async(city)=>{
+    toggleLoader();
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+    toggleLoader();
+    return data
+}
+
+// Tratamento de erro
+const showErrorMessage = () => {
+    errorMessageContainer.classList.remove("hide");
+  };
+
+  const hideInformation = () => {
+    errorMessageContainer.classList.add("hide");
+    weatherContainer.classList.add("hide");
+  
+    suggestionContainer.classList.add("hide");
+  };
+  
+const showWeatherData = async (city)=>{
+    hideInformation();
+    const data = await getWeatherData(city);
+    if (data.cod === "404") {
+        showErrorMessage();
+        return;
+      }
+    cityElement.innerText = data.name;
+    tempELement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src",apitCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windELement.innerText = `${data.wind.speed}Km/Hr`;
+
+    document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+    weatherContainer.classList.remove("hide")
+    
+}
+//Eventos
+searchBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+})
+
+cityInput.addEventListener("keyup",(e)=>{
+    if(e.code === "Enter"){
+        const city = e.target.value
+        showWeatherData(city);
+    }
+})
+
+// Sugestões
+suggestionButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const city = btn.getAttribute("id");
+  
+      showWeatherData(city);
+    });
+  });
